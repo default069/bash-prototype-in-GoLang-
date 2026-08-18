960be9c56737c7bc107613214c8a00c39edfef90
default069 <abubakir72273313@gmail.com>
2026-08-18 21:45:31 +0500

    commands ls done

--- bash-in-golang.go
+++ bash-in-golang.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
+	var command string
+
 	fmt.Println("Welcome to Bash in GoLang")
+	fmt.Println("Please choos operator ls, cd, mkdir, touch ")
+	fmt.Scan(&command)
+
+	if command == "ls"{
+		ls(".")
+	}
+}
+
+func ls(path string){
+	entries , err := os.ReadDir(path)
+	if err != nil{
+		fmt.Println("directory read error:", err)
+		return
+	}
+
+	for _, entry := range entries{
+		if entry.IsDir(){
+			fmt.Printf("%s\n", entry.Name())
+		}else{
+			fmt.Printf("%s\n",entry.Name())
+		}
+	}
 }
