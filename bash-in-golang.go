d9a688cbe3416f84be6e2daabc4466d38b0c4da7
default069 <abubakir72273313@gmail.com>
2026-08-18 22:00:27 +0500

    done func cd

--- bash-in-golang.go
+++ bash-in-golang.go
@@ -9,12 +9,21 @@ func main(){
 	var command string
 
 	fmt.Println("Welcome to Bash in GoLang")
+	for{
+
+	
 	fmt.Println("Please choos operator ls, cd, mkdir, touch ")
 	fmt.Scan(&command)
 
 	if command == "ls"{
 		ls(".")
+	}else if command == "cd"{
+		var path string
+		fmt.Println("Enter the path to the folder:")
+		fmt.Scan(&path)
+		cd(path)
 	}
+  }
 }
 
 func ls(path string){
@@ -31,4 +40,16 @@ func ls(path string){
 			fmt.Printf("%s\n",entry.Name())
 		}
 	}
+}
+func cd(path string){
+	err := os.Chdir(path)
+	if err != nil {
+		fmt.Println("Directory change error:")
+		return
+	}
+
+	currentDir, err := os.Getwd()
+	if err == nil {
+		fmt.Println("We have successfully transitioned to:", currentDir)
+	} 
 }
